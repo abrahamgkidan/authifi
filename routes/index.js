225a413877cc3b20b64b2ba71796545e64b3bdcf
Abraham Gebrekidan <abrahamkgebrekidan@yahoo.com>
2021-03-20 04:19:25 -0700

    Changed welcome title to AuthiFi

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const testRoute = router.get("/", function (req, res) {
   res.send(
-    '<h2 style="text-align: center; fontWeight: 500; padding: 20px">Welcome to MallPerty!</h2>'
+    '<h2 style="text-align: center; fontWeight: 500; padding: 20px">Welcome to AuthiFi!</h2>'
   );
 });
 
